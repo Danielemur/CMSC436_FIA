c62a1eafe7a816536425d5d710cb7325b3e71d40
Tim Chen <timchen.chen@gmail.com>
2018-12-02 17:18:44 -0500

    added MapFragment and updated Manifest and other XML files

--- app/src/main/java/com/example/daniel/tastet/MapsFragment.java
+++ app/src/main/java/com/example/daniel/tastet/MapsFragment.java
@@ -7,11 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MapsFragment extends Fragment {
-    @Nullable
+    MapView mapView;
+    GoogleMap googleMap;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_fragment,null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        mapView = (MapView) view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                LatLng ltlng = new LatLng(38.9897, -76.9378);
+                float zoom = 10;
+                UiSettings uiSetting = map.getUiSettings();
+                uiSetting.setZoomControlsEnabled(true);
+                uiSetting.setCompassEnabled(true);
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(ltlng, zoom));
+                googleMap = map;
+            }
+        });
+
     }
 }
