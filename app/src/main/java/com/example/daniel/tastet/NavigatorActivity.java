34f6a051a8e1e31ce914dd241c753d0c446d5acc
Michael <mikeliumeng@gmail.com>
2018-12-02 13:20:35 -0500

    Added Fragments for all screens

--- app/src/main/java/com/example/daniel/tastet/NavigatorActivity.java
+++ app/src/main/java/com/example/daniel/tastet/NavigatorActivity.java
@@ -3,45 +3,55 @@ package com.example.daniel.tastet;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class NavigatorActivity extends AppCompatActivity {
+public class NavigatorActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_map:
-                    mTextMessage.setText(R.string.title_map);
-                    return true;
-                case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        BottomNavigationView navigation =  findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
 
+        //initialize to home when just starting up
+        loadFragment(new HomeFragment());
+
+    }
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        //switch to the selected menu option
+        switch(menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_search:
+                fragment = new SearchFragment();
+                break;
+            case R.id.navigation_map:
+                fragment = new MapsFragment();
+                break;
+            case R.id.navigation_settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
