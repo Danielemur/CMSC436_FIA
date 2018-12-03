bf63456d39bd1eeebe474068c32849b494149e59
Daniel Kelly <dk97@comcast.net>
2018-12-02 21:06:34 -0500

    WIP on AddStoreActivity.

--- app/src/main/java/com/example/daniel/tastet/NavigatorActivity.java
+++ app/src/main/java/com/example/daniel/tastet/NavigatorActivity.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
 
 public class NavigatorActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private static final String TAG = "TasteT";
+    public static final String TAG = "TasteT";
     private static final int ADD_STORE_REQUEST = 0;
 
 
@@ -79,7 +79,12 @@ public class NavigatorActivity extends AppCompatActivity implements BottomNaviga
     @Override	
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i(TAG, "Entered onActivityResult()");
-
+        if (resultCode == RESULT_OK && requestCode == ADD_STORE_REQUEST) {
+            String locationName = data.getStringExtra(AddStoreActivity.LOCATION_NAME);
+            String locationAddress = data.getStringExtra(AddStoreActivity.LOCATION_ADDRESS);
+            String locationType = data.getStringExtra(AddStoreActivity.LOCATION_TYPE);
+            Log.i(TAG, "Name: " + locationName + "\nAddress: " + locationAddress + "\nType: " + locationType);
+        }
     }
 
     private boolean loadFragment(){
