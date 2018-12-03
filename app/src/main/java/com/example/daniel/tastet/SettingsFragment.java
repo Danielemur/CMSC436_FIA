33b78d2eee0ff753fc5953d45d4ed0fb0ffd655c
Tim Chen <timchen.chen@gmail.com>
2018-12-03 00:57:51 -0500

    preferences page opens now

--- app/src/main/java/com/example/daniel/tastet/SettingsFragment.java
+++ app/src/main/java/com/example/daniel/tastet/SettingsFragment.java
@@ -12,18 +12,18 @@ import android.view.ViewGroup;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-    @Nullable
+  /*  @Nullable
     @Override
    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.settings_fragment,null);
-    }
+    }*/
 
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         //Load preferences from XML file
         addPreferencesFromResource(R.xml.preferences);
-
+/*
         //Preference mNotificationPreference = getPreferenceManager().findPreference("notification_key");
         //Preference mDefaultLocation = getPreferenceManager().findPreference("default_location_key");
         //Preference mDefaultZoom = getPreferenceManager().findPreference("zoom_key");
@@ -33,26 +33,26 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         SharedPreferences prefs = getActivity().getSharedPreferences("SettingsFile", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
 
-        /*If the default location hasn't been set, set it*/
+        *//*If the default location hasn't been set, set it*//*
         if (!prefs.getString("default_location_key", "missing").equals("missing")) {
-            /*Add default location to SettingsFile*/
+            *//*Add default location to SettingsFile*//*
             editor.putString("default_location_key", "home");
             editor.apply();
         }
 
-        /*If the default zoom setting hasn't been set, set it*/
+        *//*If the default zoom setting hasn't been set, set it*//*
         if (!prefs.getString("default_zoom_key", "missing").equals("missing")) {
-            /*Add default zoom level for map to SettingsFile*/
+            *//*Add default zoom level for map to SettingsFile*//*
             editor.putInt("default_zoom_preference", 10);
             editor.apply();
         }
 
-        /*If the default zoom setting hasn't been set, set it*/
+        *//*If the default zoom setting hasn't been set, set it*//*
         if (!prefs.getString("notifications_key", "missing").equals("missing")) {
-            /*Add default notification setting to SettingsFile*/
+            *//*Add default notification setting to SettingsFile*//*
             editor.putBoolean("notification_key", true);
             editor.apply();
-        }
+        }*/
 
 
 
