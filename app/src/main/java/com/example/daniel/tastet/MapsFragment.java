6b30052b100fc2e1cae3e5dfbfd7b5f3f2c6a271
Tim Chen <timchen.chen@gmail.com>
2018-12-02 19:25:40 -0500

    created Search Bar for Map

--- app/src/main/java/com/example/daniel/tastet/MapsFragment.java
+++ app/src/main/java/com/example/daniel/tastet/MapsFragment.java
@@ -1,18 +1,32 @@
 package com.example.daniel.tastet;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.location.Geocoder;
+import android.location.Address;
+import android.util.Log;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import java.util.List;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
+import android.widget.EditText;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.TextView;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Locale;
+
 public class MapsFragment extends Fragment {
     MapView mapView;
+    EditText mapSearchBox;
     GoogleMap googleMap;
+    Address address;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,18 +40,50 @@ public class MapsFragment extends Fragment {
         mapView = (MapView) view.findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
+
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap map) {
                 LatLng ltlng = new LatLng(38.9897, -76.9378);
-                float zoom = 10;
+                float zoom = 15;
                 UiSettings uiSetting = map.getUiSettings();
                 uiSetting.setZoomControlsEnabled(true);
-                uiSetting.setCompassEnabled(true);
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(ltlng, zoom));
                 googleMap = map;
             }
         });
+
+        mapSearchBox = (EditText) view.findViewById(R.id.search);
+        mapSearchBox.setOnEditorActionListener(new OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        actionId == EditorInfo.IME_ACTION_GO ||
+                        event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    // hide virtual keyboard
+                    try {
+                        Geocoder geocoder = new Geocoder(getActivity().getApplicationContext(), Locale.US);
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(mapSearchBox.getWindowToken(), 0);
+                        List<Address> results = geocoder.getFromLocationName(mapSearchBox.getText().toString(), 1);
+                        if (results.size() == 0) {
+                            return false;
+                        }
+                        address = results.get(0);
+                        Log.d("yeet", mapSearchBox.getText().toString());
+                        LatLng ltlng = new LatLng(address.getLatitude(), address.getLongitude());
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ltlng, 15));
+                        mapSearchBox.setText("", TextView.BufferType.EDITABLE);
+                    } catch (Exception e){
+                        return false;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
     }
 }
