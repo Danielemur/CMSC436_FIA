da90c823e9ff97f5bdcf0c3b19c14711516eb5d2
Daniel Kelly <dk97@comcast.net>
2018-12-03 18:27:27 -0500

    Added onCreate method to StorePage and ReviewPage.

--- app/src/main/java/com/example/daniel/tastet/ReviewPageActivity.java
+++ app/src/main/java/com/example/daniel/tastet/ReviewPageActivity.java
@@ -1,6 +1,12 @@
 package com.example.daniel.tastet;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ReviewPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/daniel/tastet/StorePageActivity.java
+++ app/src/main/java/com/example/daniel/tastet/StorePageActivity.java
@@ -1,6 +1,12 @@
 package com.example.daniel.tastet;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class StorePageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
