6cfad29ea6385d1d3ad2a9ab960184e663cda62f
Dan Selzer <dselzer@optonline.net>
2018-12-04 03:31:25 -0500

    Add Review Firebase logic

--- app/src/main/java/com/example/daniel/tastet/StorePageActivity.java
+++ app/src/main/java/com/example/daniel/tastet/StorePageActivity.java
@@ -3,26 +3,31 @@ package com.example.daniel.tastet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class StorePageActivity extends Activity {
     private static final int ADD_REVIEW_REQUEST = 1;
-
+    private Store data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.store_page_activity);
 
-        Store s = new Store(this.getIntent());
+        data = new Store(this.getIntent());
 
 
         TextView storeNameView = this.findViewById(R.id.store_name_entry);
@@ -30,9 +35,9 @@ public class StorePageActivity extends Activity {
         TextView storeTypeView = this.findViewById(R.id.store_type_entry);
         RatingBar storeRatingView = this.findViewById(R.id.store_overall_rating);
 
-        storeNameView.setText(s.getLocationName());
-        storeAddressView.setText(s.getLocationAddress());
-        storeTypeView.setText(s.getLocationType());
+        storeNameView.setText(data.getLocationName());
+        storeAddressView.setText(data.getLocationAddress());
+        storeTypeView.setText(data.getLocationType());
         //TODO
         storeRatingView.setRating(3.5f);
 
@@ -53,16 +58,43 @@ public class StorePageActivity extends Activity {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             switch(requestCode) {
                 case ADD_REVIEW_REQUEST: {
-                    String reviewTitle = data.getStringExtra(AddReviewActivity.REVIEW_TITLE).toString();
-                    String reviewUser = data.getStringExtra(AddReviewActivity.REVIEW_USER).toString();
-                    float reviewOverall = data.getFloatExtra(AddReviewActivity.REVIEW_OVERALL, 3);
-                    float reviewFreshness = data.getFloatExtra(AddReviewActivity.REVIEW_FRESHNESS, 3);
-                    float reviewTaste = data.getFloatExtra(AddReviewActivity.REVIEW_TASTE, 3);
-                    float reviewPrice = data.getFloatExtra(AddReviewActivity.REVIEW_PRICE, 3);
-                    String reviewText = data.getStringExtra(AddReviewActivity.REVIEW_TEXT).toString();
+                    final String reviewTitle = data.getStringExtra(AddReviewActivity.REVIEW_TITLE).toString();
+                    final String reviewUser = data.getStringExtra(AddReviewActivity.REVIEW_USER).toString();
+                    final float reviewOverall = data.getFloatExtra(AddReviewActivity.REVIEW_OVERALL, 3);
+                    final float reviewFreshness = data.getFloatExtra(AddReviewActivity.REVIEW_FRESHNESS, 3);
+                    final float reviewTaste = data.getFloatExtra(AddReviewActivity.REVIEW_TASTE, 3);
+                    final float reviewPrice = data.getFloatExtra(AddReviewActivity.REVIEW_PRICE, 3);
+                    final String reviewText = data.getStringExtra(AddReviewActivity.REVIEW_TEXT).toString();
 
-                    String idOne = UUID.randomUUID().toString();
-                    DatabaseReference myRef = database.getReference(idOne);
+                    String idOne = StorePageActivity.this.data.getHashKey();
+                    final DatabaseReference myRef = database.getReference(idOne);
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                             @Override
+                                                             public void onDataChange(DataSnapshot snapshot) {
+                                                                 Map<String, Object> currentStore = (Map<String, Object>) snapshot.getValue();
+                                                                 ArrayList<HashMap<String,Object>> reviews = (ArrayList<HashMap<String,Object>>) currentStore.get("Reviews");
+                                                                 HashMap<String,Object> newReview = new HashMap<String,Object>();
+
+                                                                 newReview.put("Body",reviewText);
+                                                                 newReview.put("Date",new java.util.Date().toString());
+                                                                 newReview.put("Freshness",reviewFreshness);
+                                                                 newReview.put("Name",reviewUser);
+                                                                 newReview.put("Overall",reviewOverall);
+                                                                 newReview.put("Price",reviewPrice);
+                                                                 newReview.put("Taste",reviewTaste);
+                                                                 newReview.put("Title",reviewTitle);
+
+                                                                 reviews.add(newReview);
+                                                                 currentStore.put("Reviews", reviews);
+                                                                 myRef.setValue(currentStore);
+                                                             }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            System.out.println("The read failed: " + databaseError.getCode());
+                        }
+                    });
+
                     Map<String, Object> result = new HashMap<>();
                     Map<String, String> reviews = new HashMap<>();
                     result.put("Name", reviewTitle);
